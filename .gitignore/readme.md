3543c175fd5eb6c9fd30404c1e2e4cefe07c809c
chdoff <34540855+chdoff@users.noreply.github.com>
2017-12-14 17:05:49 +0530

    initial

---
+++ .gitignore/readme.md
@@ -0,0 +1,19 @@
+#Chandigarh
+
+Version 1.0
+  
+Header Image:
+=============
+aerial-shot-of-gray-dock-on-body-of-water
+https://www.pexels.com/photo/aerial-shot-of-gray-dock-on-body-of-water-697313/
+
+Blog Images:
+=============
+adventure-altitude-climb-clouds
+https://www.pexels.com/photo/adventure-altitude-climb-clouds-238619/
+
+woman-in-black-yellow-and-white-floral-button-up-shirt-holding-smartphone-wearing-aviator-sunglasses-near-body-of-water
+https://www.pexels.com/photo/woman-in-black-yellow-and-white-floral-button-up-shirt-holding-smartphone-wearing-aviator-sunglasses-near-body-of-water-681793/
+
+adventure-climber-cold-hiking
+https://www.pexels.com/photo/adventure-climber-cold-hiking-236959/
---
+++ .gitignore/template-parts/post/content-home.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <?php
+    if ( is_sticky() && is_home() ) :
+        echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
+    endif;
+    ?>
+    <div class="post-thumb">
+        <?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
+            <div class="post-thumbnail">
+                <?php if (has_post_thumbnail()) : ?>
+                    <a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>"><?php the_post_thumbnail('chd-off-thumb'); ?></a>
+                <?php else: ?>
+                    <a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>"><img src="<?php echo get_template_directory_uri()."/assets/images/placeholder2.jpg"; ?>"></a>
+                <?php endif; ?>
+            </div><!-- .post-thumbnail -->
+        <?php endif; ?>
+    </div>
+
+    <div class="post-content">
+        <div class="entry-content">
+           <header class="entry-header">
+				<h1 class="entry-title title-font"><a class="hvr-underline-reveal" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+               <?php if ( 'post' === get_post_type() ) {
+               echo '<div class="entry-meta">';
+                   if ( is_single() ) {
+                   twentyseventeen_posted_on();
+                   } else {
+                   echo twentyseventeen_time_link();
+                   twentyseventeen_edit_link();
+                   };
+                   echo '</div><!-- .entry-meta -->';
+               };?>
+               <span class="entry-excerpt"><?php echo substr(get_the_excerpt(),0,300).(get_the_excerpt() ? "..." : "twentyseventeen" ); ?></span>
+               <span class="readmore"><a href="<?php the_permalink() ?>"><?php _e('Read More','twentyseventeen'); ?></a></span>
+           </header><!-- .entry-header -->
+        </div><!-- .entry-content -->
+    </div>
+    <?php
+    if ( is_single() ) {
+        twentyseventeen_entry_footer();
+    }
+    ?>
+
+</article><!-- #post-## -->
