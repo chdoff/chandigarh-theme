66f4f837c5505948e241d35d3d475d93e5dcf1cc
chdoff <34540855+chdoff@users.noreply.github.com>
2017-12-15 15:58:06 +0530

    Create site-info.php

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen, Override by Chandigarh WordPress Theme.
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( 'https://github.com/chdoff/chandigarh-theme' ); ?>">
+		<?php _e('Proudly powered by Chandigarh Theme', 'chandigarh' ); ?>
+	</a>
+</div><!-- .site-info -->
